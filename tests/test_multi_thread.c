05a7c04ca520fb2584c69b5833905ff5508b7d8f
Tyler Connel <tyler@schedmd.com>
2024-08-28 14:20:12 -0700

    Add empty test to start checking multithreading behaviors

---
+++ tests/test_multi_thread.c
@@ -0,0 +1,19 @@
+#ifdef NDEBUG
+#undef NDEBUG
+#endif
+#include <assert.h>
+#include <limits.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "config.h"
+
+#include "json.h"
+#include "parse_flags.h"
+
+int main(int argc, char **argv)
+{
+	return EXIT_SUCCESS;
+}
