844c096194e78a46fc385eee45f69d02c8b0c9b1
Tyler Connel <tyler@schedmd.com>
2024-08-28 16:26:50 -0700

    Update test_multi_thread to resemble test2 and add check_mem output of test_multi_thread and test2

--- tests/test_multi_thread.c
+++ tests/test_multi_thread.c
@@ -1,8 +1,7 @@
-#ifdef NDEBUG
-#undef NDEBUG
-#endif
+
 #include <assert.h>
 #include <limits.h>
+#include <pthread.h>
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -13,7 +12,30 @@
 #include "json.h"
 #include "parse_flags.h"
 
+static void *thread_do(void *)
+{
+	json_object *new_obj;
+
+	new_obj = json_tokener_parse(
+	    "/* more difficult test case */"
+	    "{ \"glossary\": { \"title\": \"example glossary\", \"GlossDiv\": { \"title\": \"S\", "
+	    "\"GlossList\": [ { \"ID\": \"SGML\", \"SortAs\": \"SGML\", \"GlossTerm\": \"Standard "
+	    "Generalized Markup Language\", \"Acronym\": \"SGML\", \"Abbrev\": \"ISO 8879:1986\", "
+	    "\"GlossDef\": \"A meta-markup language, used to create markup languages such as "
+	    "DocBook.\", \"GlossSeeAlso\": [\"GML\", \"XML\", \"markup\"] } ] } } }");
+
+	json_object_put(new_obj);
+
+	return NULL;
+}
+
 int main(int argc, char **argv)
 {
+	const int num_th = 10;
+	pthread_t pth[num_th];
+	for (int idx = 0; idx < num_th; idx++)
+		assert(-1 != pthread_create(&pth[idx], NULL, thread_do, NULL));
+	for (int idx = 0; idx < num_th; idx++)
+		assert(-1 != pthread_join(pth[idx], NULL));
 	return EXIT_SUCCESS;
 }
