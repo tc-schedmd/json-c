8d134ee6eb1715629e73994b05a3c0f4f4c701e7
Tyler Connel <tyler@schedmd.com>
2024-09-03 11:13:17 -0700

    update

--- tests/test_multi_thread.c
+++ tests/test_multi_thread.c
@@ -16,15 +16,18 @@ static void *thread_do(void *)
 {
 	json_object *new_obj;
 
-	new_obj = json_tokener_parse(
-	    "/* more difficult test case */"
-	    "{ \"glossary\": { \"title\": \"example glossary\", \"GlossDiv\": { \"title\": \"S\", "
-	    "\"GlossList\": [ { \"ID\": \"SGML\", \"SortAs\": \"SGML\", \"GlossTerm\": \"Standard "
-	    "Generalized Markup Language\", \"Acronym\": \"SGML\", \"Abbrev\": \"ISO 8879:1986\", "
-	    "\"GlossDef\": \"A meta-markup language, used to create markup languages such as "
-	    "DocBook.\", \"GlossSeeAlso\": [\"GML\", \"XML\", \"markup\"] } ] } } }");
-
-	json_object_put(new_obj);
+	/* for (long int idx = 0; idx < 1000; idx++) { */
+	for (;;) {
+		new_obj = json_tokener_parse(
+					     "/* more difficult test case */"
+					     "{ \"glossary\": { \"title\": \"example glossary\", \"GlossDiv\": { \"title\": \"S\", "
+					     "\"GlossList\": [ { \"ID\": \"SGML\", \"SortAs\": \"SGML\", \"GlossTerm\": \"Standard "
+					     "Generalized Markup Language\", \"Acronym\": \"SGML\", \"Abbrev\": \"ISO 8879:1986\", "
+					     "\"GlossDef\": \"A meta-markup language, used to create markup languages such as "
+					     "DocBook.\", \"GlossSeeAlso\": [\"GML\", \"XML\", \"markup\"] } ] } } }");
+		printf("new_obj.to_string()=%s\n", json_object_to_json_string(new_obj));
+		json_object_put(new_obj);
+	}
 
 	return NULL;
 }
